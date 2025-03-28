34b27cbfc100a8c15e479ba869d950434ee34549
ItsYad <ziyadraziq01@gmail.com>
2025-03-28 14:13:35 +0700

    Tugas 3

---
+++ src/LinkedList/LinkedList/Perpustakaan/Buku.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList.Perpustakaan
+{
+    public class Buku
+    {
+        public string Judul { get; }
+        public string Penulis { get; }
+        public int Tahun { get; }
+
+        public Buku(string judul, string penulis, int tahun)
+        {
+            Judul = judul;
+            Penulis = penulis;
+            Tahun = tahun;
+        }
+    }
+}
